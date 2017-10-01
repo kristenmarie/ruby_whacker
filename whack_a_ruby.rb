917bec11f9b42d1f1de1a0d06e29f989caa603d6
Kristen Kulha <kristen.m.kulha@gmail.com>
2017-09-30 17:17:31 -0700

    Add Game Over when timer runs out.

--- whack_a_ruby.rb
+++ whack_a_ruby.rb
@@ -16,26 +16,32 @@ class WhackARuby < Gosu::Window
     @hit = 0
     @font = Gosu::Font.new(30)
     @score = 0
+    @playing = true
   end
 
   # Update means animate!
   def update
-    @x += @velocity_x
-    @y += @velocity_y
-    # When the ruby hits edge of game window
-    if @x + @width / 2 > 800 || @x - @width / 2 < 0
-      @velocity_x *= -1
+    if @playing
+      @x += @velocity_x
+      @y += @velocity_y
+      # When the ruby hits edge of game window
+      if @x + @width / 2 > 800 || @x - @width / 2 < 0
+        @velocity_x *= -1
+      end
+      if @y + @height / 2 > 600 || @y - @height / 2 < 0
+        @velocity_y *= -1
+      end
+      # Decrease visible once each frame
+      @visible -= 1
+      # Random chance visible can be visible for 30 frames.
+      if @visible < -10 && rand < 0.01
+        @visible = 30
+      end
+      @time_left = (100 - (Gosu.milliseconds / 1000))
+      if @time_left < 0
+        @playing = false
+      end
     end
-    if @y + @height / 2 > 600 || @y - @height / 2 < 0
-      @velocity_y *= -1
-    end
-    # Decrease visible once each frame
-    @visible -= 1
-    # Random chance visible can be visible for 30 frames.
-    if @visible < -10 && rand < 0.01
-      @visible = 30
-    end
-    @time_left = (100 - (Gosu.milliseconds / 1000))
   end
 
   def draw
@@ -56,16 +62,23 @@ class WhackARuby < Gosu::Window
     @hit = 0
     @font.draw(@score.to_s, 700, 20, 2)
     @font.draw(@time_left.to_s, 20, 20, 2)
+    # GAME OVER
+    unless @playing
+      @font.draw('Game Over', 300, 300, 3)
+      @visible = 20
+    end
   end
 
   def button_down(id)
-    if(id == Gosu::MsLeft)
-      if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && @visible >= 0
-        @hit = 1
-        @score += 5
-      else
-        @hit = -1
-        @score -= 1
+    if @playing
+      if(id == Gosu::MsLeft)
+        if Gosu.distance(mouse_x, mouse_y, @x, @y) < 50 && @visible >= 0
+          @hit = 1
+          @score += 5
+        else
+          @hit = -1
+          @score -= 1
+        end
       end
     end
   end
